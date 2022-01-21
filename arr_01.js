9277e355627be92de8a981481e1c47b064bb3945
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-01-21 15:34:40 +0100

    Add files via upload

---
+++ arr_01.js
@@ -0,0 +1,22 @@
+/*********   Theorie **********/
+
+
+/*
+Aufgabe:
+Erstellen Sie eine JS-Struktur, die Ihnen den folgenden String 
+einer HTML-Seite ausgibt:
+<html><head></head><body><h1></h1><p></p></body></html>
+Verwenden Sie dafür die untenstehenden Arrays
+*/
+
+const controls = ["<", "</", ">"];
+const tags = ["html","head","head","body","h1","h1","p","p","body","html"];
+let stack = [];
+
+
+
+// Modul: Ausgabe | Test
+//output("hi");
+function output(outputData) {
+    console.log(outputData);
+}
