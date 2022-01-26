f568b43e49f712e0f4bbaee6dc2cd987aa0b0053
javad290 <javad290@gmail.com>
2022-01-26 08:44:05 +0100

    objeckt

--- arr_01.js
+++ arr_01.js
@@ -1,5 +1,24 @@
 /*********   Theorie **********/
 
+// push() / pop()
+// push --> Daten rein ... (+)
+// let arr = [];
+// output(arr);
+// arr.push("Ich");
+// output(arr);
+// arr.push("bin");
+// output(arr);
+// arr.push("Max");
+// output(arr);
+
+// pop() --> Daten raus ... (-)
+// output(arr.pop());
+// output(arr);
+// output(arr.pop());
+// output(arr);
+// output(arr.pop());
+// output(arr);
+
 
 /*
 Aufgabe:
@@ -9,11 +28,68 @@ einer HTML-Seite ausgibt:
 Verwenden Sie dafür die untenstehenden Arrays
 */
 
-const controls = ["<", "</", ">"];
-const tags = ["html","head","head","body","h1","h1","p","p","body","html"];
+const cobj      = {open_o:"<",close_o:"</",close:">"};
+const controls  = ["<", "</", ">"];
+const tags = [  "html","head","head","body",
+                "h1","h1",
+                "p","p",
+                "ul","li","li","li","li","li","li","ul",
+                "p","p",
+                "body","html"
+            ];
+
 let stack = [];
 
+// Modul: HTML-Synthese | Test
+output(getHTML());
+function getHTML() {
 
+    let htmlStr = "";
+
+    for (let i = 0; i < tags.length; i++) {
+        if (isOpenElement(tags[i])) { 
+            htmlStr += getElement(tags[i],"open");
+        } else {
+            htmlStr += getElement(tags[i],"close");
+        }
+    }
+
+    return htmlStr;
+}
+
+function isOpenElement(tag) {
+    
+    // tag liegt nicht oben! --> neu, open
+    let cond = (tag != stack[stack.length-1])  
+    
+    if (cond) {  // open
+        stack.push(tag);
+        // output(stack);
+        return true;
+    } else {  // close
+        stack.pop();
+        // output(stack);
+        return false;
+    }
+     
+}
+
+// Modul: Zusammenbau der Elements: <tagStr> --> Tests:
+// output(getElement(tags[1],"open"));
+// output(getElement(tags[1],"close"));
+// output(getElement(tags[1]));
+function getElement(tag,op) {
+    switch (op) {
+        case "open": 
+            // return controls[0] + tag + controls[2];
+            return cobj.open_o + tag + cobj.close;
+        case "close":
+            // return controls[1] + tag + controls[2];
+            return cobj.close_o + tag + cobj.close;
+        default:
+          return "#!?";
+    }
+}
 
 // Modul: Ausgabe | Test
 //output("hi");
